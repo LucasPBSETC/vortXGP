49e54cfb10be4971f6d4417bbc472f738325dde0
LucasPBSETC <101125964+LucasPBSETC@users.noreply.github.com>
2022-03-14 14:55:42 +0000

    TestingSphinx

---
+++ src/main.py
@@ -0,0 +1,30 @@
+
+def func1(arg1, arg2):
+    """Function that adds two arguments.
+    :param arg1: First value.
+    :type arg1: int
+
+    :param arg2: Second value.
+    :type arg2: int
+
+    :returns: arg1 + arg2
+    """
+    return (arg1+arg2)
+
+def func2(arg):
+    """Function that transform a value into its negativ.
+    :param arg: Argument that will be reversed.
+    :type arg: int
+
+    :returns: -arg
+    """
+
+    return (-arg)
+
+if __name__ == "__main__":
+    a = 10
+    b = 667 
+
+    print(func1)
+    print("\n")
+    print(func2)
